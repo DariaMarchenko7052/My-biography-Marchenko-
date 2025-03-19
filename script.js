a68d7ae90002e83e810ab4a2897ffdea48a64204
DariaMarchenko7052 <dashamarchenko119@gmail.com>
2025-03-19 16:20:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
- /*
-  This is your site JavaScript code - you can add interactivity!
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
+ const express = require('express');
+ const app = express();
+ const PORT = 3000;
 
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
+    // API endpoint
+    app.get('/api', (req, res) => {
+                                     res.send('Hello API World from Server!');
+                                  });
 
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
+    // Serve client page
+    app.get('/', (req, res) => {
+                                res.sendFile(__dirname + '/mtable.html');
+                               });
+
+     app.listen(PORT, '0.0.0.0', () => {
+                                          console.log(`Server running at ${PORT}`);
+                                        });
